c3623403060e41f740c26af2dd2f25a0d946a3ed
Piotr A. Felisiak <piotr.felisiak@yandex.com>
2026-08-05 09:45:18 +0200

    Delete chapter_04_stud directory

--- chapter_04_stud/make_wind_parameters.m
+++
@@ -1,8 +0,0 @@
-windStatic = [-3;2;0]; % zeros(3,1); % [20;-8;2]; % 
-windsigma = [3;3;3]; % zeros(3,1); % 
-windL = [533 533 533]';
-gustsNoisePower = 0.1;
-gustsNoiseCorrelationTime = 2;
-gusts_seeds = [             1   :       n_sim;
-                n_sim   +   1   :   2 * n_sim;
-                2*n_sim +   1   :   3 * n_sim];
